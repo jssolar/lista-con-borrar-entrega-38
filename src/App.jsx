97113c398aeb0d1dc0b2840557691027f0c1d565
Juan Solar <juansolarsolar@yahoo.com>
2023-07-13 16:12:39 -0400

    trabajando en estilos, falta agregar boton de realizado y eliminar todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { AiFillCheckCircle, AiFillDelete } from "react-icons/ai";
-import'./components/App.css'
+import "./components/App.css";
 const App = () => {
   const [task, setTask] = useState("");
   const [newTask, setNewTask] = useState([]);
@@ -23,50 +23,54 @@ const App = () => {
   //========= aqui debo crear una funct que elimine la tarea que yo quiera, debo cre3ar un nuevo array sin elemento eliminado, lo puedo hacer con filter o slice, que son dos metodos que crean un nuevo arr y no modifican el original =========//
 
   const handleDelete = (borrar) => {
+    const eliminar = newTask.filter((eliminar, indice) => {
+      return indice !== borrar;
+    });
 
-    const eliminar = newTask.filter((eliminar, indice) => { 
-      return indice !== borrar}) 
-    
-      setNewTask(eliminar)
-    } 
-  
+    setNewTask(eliminar);
+  };
 
   return (
     <>
-      <div className="contenedor md-12 bg-black">
-        <form onSubmit={handleSubmit}>
+      <div className="contenedor container-fluid lg-12 bg-black ">
+        <form onSubmit={handleSubmit} className="container col-md-12">
           <input
             type="text"
-            className="input m-3"
+            className="input row-md-8"
             onChange={handleChange}
             value={task}
           />
-          <button
-            className="boton btn btn-dark d-inline m-1"
-            onClick={handleTask}
-          >
-            Add Task
-          </button>
-          <button className="boton btn btn-dark d-inline m-1">
-            {newTask.length}
-          </button>
-          <button
-            className="boton btn btn-danger d-inline m-1"
-            onClick={handleTask}
-          >
-            Delete All
-          </button>
-          <ul className="contenedor-lista">
+          <div className=" d-inline">
+            <button
+              className="boton btn btn-dark d-inline m-1"
+              onClick={handleTask}
+            >
+              Add Task
+            </button>
+            <button className="boton btn btn-dark d-inline m-1">
+              {newTask.length}
+            </button>
+            <button
+              className="boton btn btn-danger d-inline m-1"
+              onClick={handleTask}
+            >
+              Delete All
+            </button>
+          </div>
+          <ul className="container">
             {
               newTask.length > 0 ? ( // condicion dice si hay o no tareas
                 newTask.map((tarea, index) => (
-                  <li className="lista p-1 text-danger" key={index}>
+                  <li
+                    className="list-group-item lista p-1 text-danger"
+                    key={index}
+                  >
                     <AiFillCheckCircle /> {tarea}{" "}
                     <AiFillDelete onClick={() => handleDelete(index)} />
                   </li>
                 ))
               ) : (
-                <span>No hay tareas</span>
+                <span className="no-hay-tareas">No hay tareas</span>
               )
               // a cada elemento tarea ejecuta enta funcion (lo que sigue despues de la flecha)
             }
